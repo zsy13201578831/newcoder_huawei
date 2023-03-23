391ee078946c2aa6c8a87ef07af32113cdb776b4
zhangxiaoyu <690066594@qq.com>
2023-03-23 19:54:08 +0800

    HJ7 是取近似值

--- HJ5_1.cpp
+++ HJ5_1.cpp
@@ -1,20 +1,20 @@
-#include<iostream>
-using namespace std;
-
-
-int main() {
-	string s;
-	while (cin >> s) {
-		int ans(0);
-		for (int i = 2; i < s.size(); i++) {
-			if (isdigit(s[i])) {
-				ans = ans * 16 + s[i] - '0';
-			}
-			else {
-				ans = ans * 16 + 10 + s[i] - 'A';
-			}
-		}
-		cout << ans << endl;
-	}
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//
+//int main() {
+//	string s;
+//	while (cin >> s) {
+//		int ans(0);
+//		for (int i = 2; i < s.size(); i++) {
+//			if (isdigit(s[i])) {
+//				ans = ans * 16 + s[i] - '0';
+//			}
+//			else {
+//				ans = ans * 16 + 10 + s[i] - 'A';
+//			}
+//		}
+//		cout << ans << endl;
+//	}
+//	return 0;
+//}
